99e6428f22859aed5402e00dedcd877dc449619d
Andris Grishan <andris.grishan@gmail.com>
2020-01-26 22:30:48 +0200

    Controllers

---
+++ src/StudyPlanManager/Controllers/AvailableStudyCoursesController.cs
@@ -0,0 +1,23 @@
+﻿using StudyPlanManager.Logic;
+using StudyPlanManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace StudyPlanManager.Controllers
+{
+    public class AvailableStudyCoursesController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<StudyCourse> GetAvailableStudyCourses()
+        {
+            return SettingManager.Instance.AvailableStudyCourses;
+        }
+
+        [HttpPost]
+        public void SetAvailableStudyCourses(List<StudyCourse> studyCourses)
+        {
+            SettingManager.Instance.AvailableStudyCourses = studyCourses;
+        }
+    }
+}
